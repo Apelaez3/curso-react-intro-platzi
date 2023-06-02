c882a11dc3154eaac0325dc80dfad39c143c9685
Jader Pelaez <j.pelaezg@uniandes.edu.co>
2023-06-01 22:45:07 -0500

    lista de items

---
+++ src/TodoCounter.js
@@ -0,0 +1,9 @@
+function TodoCounter(){
+    return(
+        <h1>
+            Has completado 3 de 5 todo's
+        </h1>
+    );
+}
+
+export {TodoCounter};
---
+++ src/TodoItem.js
@@ -0,0 +1,11 @@
+function TodoItem(){
+    return(
+      <li>
+        <span>v</span>
+        <p>texto</p>
+        <span>X</span>
+      </li>
+    );
+  }
+
+  export {TodoItem};
